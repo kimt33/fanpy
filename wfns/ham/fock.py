350a5fc02937ac192d1b7015b59ffbb90a0d74c1
Taewon D. Kim <kimt33@mcmaster.ca>
2018-09-17 12:14:53 -0700

    Add Fock operator
    Issues
     Need to check if the math is correct. I'm not sure if the sum over
     index k (the third index in the two electron integrals) is over the
     occupied orbitals of the reference or of the operand

---
+++ wfns/ham/fock.py
@@ -0,0 +1,127 @@
+"""Fock operator using restricted orbitals."""
+from wfns.backend import slater
+from wfns.ham.restricted_chemical import RestrictedChemicalHamiltonian
+
+
+# TODO: check the math
+class FockOperator(RestrictedChemicalHamiltonian):
+    r"""Fock operator using restricted orbitals.
+
+    .. math::
+
+        \hat{f}
+        &= \sum_{ij} h_{ij} a^\dagger_i a_j
+        + \sum_{ij} \sum_{k} (g_{ikjk} - g_{ikkj}) a^\dagger_i a^\dagger_k a_k a_j\\
+
+    where :math:`h_{ik}` is the one-electron integral, :math:`g_{ijkl}` is the two-electron
+    integral in Physicists' notation, and :math:`k` is an index over the occupied orbitals.
+
+    Attributes
+    ----------
+    energy_nuc_nuc : float
+        Nuclear-nuclear repulsion energy.
+    one_int : np.ndarray(K, K)
+        One-electron integrals.
+    two_int : np.ndarray(K, K, K, K)
+        Two-electron integrals.
+    params : np.ndarray
+        Significant elements of the anti-Hermitian matrix.
+
+    Properties
+    ----------
+    dtype : {np.float64, np.complex128}
+        Data type of the Hamiltonian.
+    nspin : int
+        Number of spin orbitals.
+    nspatial : int
+        Number of spatial orbitals.
+    nparams : int
+        Number of parameters.
+
+    Methods
+    -------
+    __init__(self, one_int, two_int, orbtype=None, energy_nuc_nuc=None)
+        Initialize the Hamiltonian
+    assign_energy_nuc_nuc(self, energy_nuc_nuc=None)
+        Assigns the nuclear nuclear repulsion.
+    assign_integrals(self, one_int, two_int)
+        Assign the one- and two-electron integrals.
+    orb_rotate_jacobi(self, jacobi_indices, theta)
+        Rotate orbitals using Jacobi matrix.
+    orb_rotate_matrix(self, matrix)
+        Rotate orbitals using a transformation matrix.
+    clear_cache(self)
+        Placeholder function that would clear the cache.
+    assign_params(self, params)
+        Transform the integrals with a unitary matrix that corresponds to the given parameters.
+    integrate_wfn_sd(self, wfn, sd, wfn_deriv=None, ham_deriv=None)
+        Integrate the Hamiltonian with against a wavefunction and Slater determinant.
+    integrate_sd_sd(self, sd1, sd2, sign=None, deriv=None)
+        Integrate the Hamiltonian with against two Slater determinants.
+
+    """
+    def integrate_sd_sd(self, sd1, sd2, sign=None, deriv=None):
+        r"""Integrate the Hamiltonian with against two Slater determinants.
+
+        .. math::
+
+            H_{\mathbf{m}\mathbf{n}} &=
+            \left< \mathbf{m} \middle| \hat{H} \middle| \mathbf{n} \right>\\
+            &= \sum_{ij}
+               h_{ij} \left< \mathbf{m} \middle| a^\dagger_i a_j \middle| \mathbf{n} \right>
+            + \sum_{i<j, k<l} g_{ijkl}
+            \left< \mathbf{m} \middle| a^\dagger_i a^\dagger_j a_l a_k \middle| \mathbf{n} \right>\\
+
+        In the first summation involving :math:`h_{ij}`, only the terms where :math:`\mathbf{m}` and
+        :math:`\mathbf{n}` are different by at most single excitation will contribute to the
+        integral. In the second summation involving :math:`g_{ijkl}`, only the terms where
+        :math:`\mathbf{m}` and :math:`\mathbf{n}` are different by at most double excitation will
+        contribute to the integral.
+
+        Parameters
+        ----------
+        sd1 : int
+            Slater Determinant against which the Hamiltonian is integrated.
+        sd2 : int
+            Slater Determinant against which the Hamiltonian is integrated.
+        sign : {1, -1, None}
+            Sign change resulting from cancelling out the orbitals shared between the two Slater
+            determinants.
+            Computes the sign if none is provided.
+            Make sure that the provided sign is correct. It will not be checked to see if its
+            correct.
+        deriv : {int, None}
+            Index of the Hamiltonian parameter against which the integral is derivatized.
+            Default is no derivatization.
+
+        Returns
+        -------
+        one_electron : float
+            One-electron energy.
+        coulomb : float
+            Coulomb energy.
+        exchange : float
+            Exchange energy.
+
+        Raises
+        ------
+        ValueError
+            If `sign` is not `1`, `-1` or `None`.
+        NotImplementedError
+            If `deriv` is not `None`.
+
+        """
+        if deriv is not None:
+            raise NotImplementedError('Orbital rotation is not implemented properly: you cannot '
+                                      'take the derivative of CI matrix elements with respect to '
+                                      'orbital rotation coefficients.')
+
+        sd1 = slater.internal_sd(sd1)
+        sd2 = slater.internal_sd(sd2)
+        diff_sd1, diff_sd2 = slater.diff_orbs(sd1, sd2)
+        diff_order = len(diff_sd1)
+
+        if diff_order == 2:
+            return 0.0, 0.0, 0.0
+
+        return super().integrate_sd_sd(sd1, sd2, sign=sign, deriv=deriv)
